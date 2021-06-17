577600a6663114603ac27efe18d2428ccb2f064b
ken@thecambiumgroup.co.uk <ken@thecambiumgroup.co.uk>
2021-06-17 16:37:45 +0100

    #41847 adding something to test TWS

--- apps/tws-next/pages/index.tsx
+++ apps/tws-next/pages/index.tsx
@@ -1,6 +1,11 @@
 import { SharedComponents } from '@cambium-ufe/shared-components';
 import React from 'react';
 
-export const Index = () => <SharedComponents title="TWS-NEXT" />;
+export const Index = () => (
+  <div>
+    <SharedComponents title="TWS-NEXT" />
+    <div>Something specific to TWS</div>
+  </div>
+);
 
 export default Index;
