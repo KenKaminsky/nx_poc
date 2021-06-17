d14fc70be881a5f9b3ee00d46bbd688ff3fe1e58
ken@thecambiumgroup.co.uk <ken@thecambiumgroup.co.uk>
2021-06-17 16:42:23 +0100

    #41847 adding something to shared components to test both projects

--- libs/shared-components/src/lib/shared-components.tsx
+++ libs/shared-components/src/lib/shared-components.tsx
@@ -7,6 +7,7 @@ export function SharedComponents(props: SharedComponentsProps) {
   return (
     <div>
       <h1>Welcome to {props.title}!</h1>
+      <h3>Subtitle in shared component</h3>
     </div>
   );
 }
